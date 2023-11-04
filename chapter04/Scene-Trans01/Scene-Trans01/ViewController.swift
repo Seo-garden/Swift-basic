0d9b565d90756287b021e59524327d07744d6a8c
seo-garden <indian4589@naver.com>
2023-11-04 16:01:18 +0900

    1104 commit

---
+++ chapter04/Scene-Trans01/Scene-Trans01/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Scene-Trans01
+//
+//  Created by 서정원 on 11/4/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func moveNext(_ sender: Any) {
+        //이동할 뷰 컨트롤러 객체를 RestorationID 정보를 이용하여 참조
+        let uvc = self.storyboard!.instantiateViewController(withIdentifier: "SecondVC")
+        //화면 전환할 때의 애니메이션 타입
+        uvc.modalTransitionStyle = UIModalTransitionStyle.coverVertical
+        //인자값으로 뷰 컨트롤러 인스턴스를 넣고 present 메소드 호출
+        self.present(uvc, animated: true)
+    }
+}
+
