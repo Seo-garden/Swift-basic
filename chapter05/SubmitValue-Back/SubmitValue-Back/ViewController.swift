94df684fa331ccf5c45e5f601b3b6397cd607493
seo-garden <indian4589@naver.com>
2023-11-13 14:20:01 +0900

    1113 readme

--- chapter05/SubmitValue-Back/SubmitValue-Back/ViewController.swift
+++ chapter05/SubmitValue-Back/SubmitValue-Back/ViewController.swift
@@ -13,21 +13,28 @@ class ViewController: UIViewController {
     @IBOutlet var resultEmail: UILabel!
     @IBOutlet var resultUpdate: UILabel!
     @IBOutlet var resultInterval: UILabel!
-    
-    var paramEmail : String?
-    var paramUpdate : Bool?
-    var paramInterval : Double?
-    
+
     override func viewWillAppear(_ animated: Bool) {
-        if let email = paramEmail {
+        let ad = UIApplication.shared.delegate as? AppDelegate
+        
+        if let email = ad?.paramEmail {
             resultEmail.text = email
         }
-        if let update = paramUpdate {
+        if let update = ad?.paramUpdate {
             resultUpdate.text = update == true ? "자동갱신" : "자동갱신안함"
         }
-        if let interval = paramInterval {
+        if let interval = ad?.paramInterval {
             resultInterval.text = "\(Int(interval))분마다"
         }
+//        if let email = paramEmail {
+//            resultEmail.text = email
+//        }
+//        if let update = paramUpdate {
+//            resultUpdate.text = update == true ? "자동갱신" : "자동갱신안함"
+//        }
+//        if let interval = paramInterval {
+//            resultInterval.text = "\(Int(interval))분마다"
+//        }
     }
 }
 
