5b51816d35bb596ccac7a6e31b623841163a0aaa
seo-garden <indian4589@naver.com>
2024-05-08 22:34:56 +0900

    ~240

--- chapter04/Scene-UnwindSegue/Scene-UnwindSegue/SecondViewController.swift
+++ chapter04/Scene-UnwindSegue/Scene-UnwindSegue/SecondViewController.swift
@@ -2,13 +2,14 @@
 //  SecondViewController.swift
 //  Scene-UnwindSegue
 //
-//  Created by 서정원 on 11/6/23.
+//  Created by 서정원 on 5/8/24.
 //
 
 import Foundation
 import UIKit
 
-@IBAction func unwindToVC(_ segue : UIStoryboardSegue) {
-    
+class SecondViewController: UIViewController {
+    @IBAction func myUnwindAction(unwindSegue: UIStoryboardSegue) {
+        
+    }
 }
-
--- chapter04/Scene-UnwindSegue/Scene-UnwindSegue/ViewController.swift
+++ chapter04/Scene-UnwindSegue/Scene-UnwindSegue/ViewController.swift
@@ -11,5 +11,15 @@ class ViewController: UIViewController {
     @IBAction func unwindToVC(_ segue : UIStoryboardSegue) {
         
     }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "SegueToB" {
+            // B 파일의 인스턴스를 가져옵니다.
+            if let bViewController = segue.destination as? SecondViewController {
+                // B 파일에 필요한 데이터를 설정합니다.
+                
+            }
+        }
+    }
 }
 
